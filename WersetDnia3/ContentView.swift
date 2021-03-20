f19d6fff08fa8d9ea7e62f3d5f8c8a2ed052be2a
Tymoteusz Stokarski <tjoelstokarski@gmail.com>
2021-03-20 05:49:51 +0100

    Initial Commit

---
+++ WersetDnia3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WersetDnia3
+//
+//  Created by Tymoteusz Stokarski on 20/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
