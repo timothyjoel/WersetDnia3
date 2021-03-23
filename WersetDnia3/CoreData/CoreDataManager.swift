c181005579f40d40a058a310b7538f2f430d6bf6
Tymoteusz Stokarski <tjoelstokarski@gmail.com>
2021-03-23 23:24:26 +0100

    add delete all data option do core data manager

--- WersetDnia3/CoreData/CoreDataManager.swift
+++ WersetDnia3/CoreData/CoreDataManager.swift
@@ -15,11 +15,42 @@ protocol CoreDataManagerProtocol {
 
 class LikedVersesDataManager: CoreDataManagerProtocol {
 
+    // MARK: - Properties
+    
     static let shared = LikedVersesDataManager()
     var entityName = "LikedVerse"
 
+    // MARK: - Initializers
     private init() {}
 
+    // MARK: - Methods
+    
+    func fetchVerses(completion: @escaping ([Verse]) -> Void) {
+        
+        guard let managedContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {
+            os_log(.fault, log: .coreData, "Failed to create App Delegate.")
+            return
+        }
+
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+ 
+        do {
+            
+            if let likedVerses = try managedContext.fetch(fetchRequest) as? [LikedVerse] {
+                os_log(.info, log: .coreData, "Fetched verses liked, count: %@.", "\(likedVerses.count)")
+                let verses = likedVerses.map { Verse(path: $0.path!, text: $0.text!) }
+                completion(verses)
+            } else {
+                print("no liked verses")
+                completion([])
+            }
+
+        } catch let error as NSError {
+            os_log(.error, log: .coreData, "Failed to load data, error: %@.", error.localizedDescription)
+        }
+
+    }
+    
     func save(_ verse: Verse) {
         guard let managedContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {
             os_log(.fault, log: .coreData, "Failed to create App Delegate.")
@@ -67,31 +98,37 @@ class LikedVersesDataManager: CoreDataManagerProtocol {
         }
 
     }
-
-    func fetchVerses(completion: @escaping ([Verse]) -> Void) {
-        
+    
+    func deleteAllData() {
         guard let managedContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {
             os_log(.fault, log: .coreData, "Failed to create App Delegate.")
             return
         }
-
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
- 
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        fetchRequest.returnsObjectsAsFaults = false
         do {
-            
-            if let likedVerses = try managedContext.fetch(fetchRequest) as? [LikedVerse] {
-                os_log(.info, log: .coreData, "Fetched verses liked, count: %@.", "\(likedVerses.count)")
-                let verses = likedVerses.map { Verse(path: $0.path!, text: $0.text!) }
-                completion(verses)
-            } else {
-                print("no liked verses")
-                completion([])
+            let results = try managedContext.fetch(fetchRequest)
+            print(results.count)
+            guard !results.isEmpty else {
+                os_log(.info, log: .coreData, "Attempted to remove all liked verses data but database was empty.")
+                return
             }
-
+            for managedObject in results {
+                if let managedObjectData: NSManagedObject = managedObject as? NSManagedObject {
+                    managedContext.delete(managedObjectData)
+                }
+            }
+            do {
+                try managedContext.save()
+                os_log(.info, log: .coreData, "Successfully deleted all liked verses and saved empty database.")
+            } catch let error as NSError {
+                os_log(.error, log: .coreData, "Failed to save empty database, error: %@", error.localizedDescription)
+            }
         } catch let error as NSError {
-            os_log(.error, log: .coreData, "Failed to load data, error: %@.", error.localizedDescription)
+            os_log(.error, log: .coreData, "Failed to delete all liked verses, error: %@", error.localizedDescription)
         }
-
     }
+    
+    
 
 }
